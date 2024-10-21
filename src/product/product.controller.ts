e47e6c2d1c0b4f106e818d0283774ca58a9920ab
wangziyi <ziyi.wang@shopee.com>
2024-10-21 11:51:13 +0800

    feat: delete popular country

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,20 +1,50 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { UserInfo } from '@/common';
+import { Token } from '@/common';
+import { JwtAuthGuard } from '@/auth/guard/jwt.guard';
+import { Action } from './product.service';
 
 @Controller('product')
+@UseGuards(JwtAuthGuard)
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Get('country/getAllCountry')
   async getCountryList() {
     const countries = await this.productService.getCountryList();
-    console.log('countries1', countries);
+
     return {
       code: 0,
       message: 'Success',
       data: {
         countries,
       },
+    };
+  }
+
+  /**
+   * @description 原意是根据action来判断增删改查，但作者认为应该符合RESTful API 设计原则,删除使用DELET方法,修改使用PUT方法
+   * @param {{ action: number; countryIds: number[] }} body
+   * @param {UserInfo} userInfo
+   * @return {*}
+   * @memberof ProductController
+   */
+  @Post('country/changePopular')
+  async changePopular(
+    @Body() body: { action: Action; countryIds: number[] },
+    @Token() userInfo: UserInfo,
+  ) {
+    const { countryIds, action } = body;
+
+    await this.productService.changePopularCountry(
+      action,
+      countryIds,
+      userInfo.email,
+    );
+    return {
+      code: 0,
+      message: 'Success',
     };
   }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -3,17 +3,131 @@ import { Country } from '@/common/models/country';
 import { Injectable } from '@nestjs/common';
 import { plainToClass } from 'class-transformer';
 
+export enum Action {
+  New,
+  SingleDelete,
+  BatchDelete,
+}
+
+const actionMap = {
+  [Action.New]: 0,
+  [Action.SingleDelete]: 1,
+  [Action.BatchDelete]: 2,
+};
+
 @Injectable()
 export class ProductService {
   constructor(private prisma: PrismaService) {}
 
   async getCountryList() {
-    const countries = await this.prisma.product_country_tab.findMany();
-    console.log(
-      'coun',
-      countries,
-      countries.map((country) => plainToClass(Country, country)),
-    );
+    const countries = await this.prisma.product_country_tab.findMany({
+      orderBy: {
+        country_sort: 'asc',
+      },
+    });
+
     return countries.map((country) => plainToClass(Country, country));
   }
+
+  async addPopularCountry(countryIds: number[], email: string) {
+    // 限制热门国家为5个
+    const count = await this.prisma.product_country_tab.count({
+      where: { is_popular: 1 },
+    });
+    if (count >= 5) throw new Error('最多只能添加5个热门国家');
+
+    const updatePromises = countryIds.map(async (countryId, index) => {
+      await this.prisma.product_country_tab.updateMany({
+        where: { country_id: countryId },
+        data: {
+          is_popular: 1,
+          gmt_modified: new Date(),
+          country_sort: count + index + 1,
+          operator: email,
+        },
+      });
+    });
+    return Promise.all(updatePromises);
+  }
+
+  async deletePopularCountry(countryIds: number[], email: string) {
+    await this.prisma.product_country_tab.updateMany({
+      where: { country_id: { in: countryIds } },
+      data: {
+        is_popular: 0,
+        gmt_modified: new Date(),
+        country_sort: 0,
+        operator: email,
+      },
+    });
+
+    const newPopularCountries = await this.prisma.product_country_tab.findMany({
+      where: {
+        country_id: { notIn: countryIds },
+        is_popular: 1,
+      },
+    });
+
+    const updatePromises = newPopularCountries.map(
+      async ({ country_id }, index) => {
+        await this.prisma.product_country_tab.updateMany({
+          where: { country_id },
+          data: {
+            gmt_modified: new Date(),
+            country_sort: index + 1,
+            operator: email,
+          },
+        });
+      },
+    );
+    return Promise.all(updatePromises);
+  }
+
+  async singleDeletePopularCountry(countryIds: number[], email: string) {
+    const records = await this.prisma.product_country_tab.findFirstOrThrow({
+      where: { country_id: countryIds[0] },
+    });
+    await this.prisma.product_country_tab.updateMany({
+      where: { country_id: countryIds[0] },
+      data: {
+        is_popular: 0,
+        gmt_modified: new Date(),
+        operator: email,
+        country_sort: 0,
+      },
+    });
+    await this.prisma.product_country_tab.updateMany({
+      where: { country_sort: { gt: records.country_sort }, is_popular: 1 },
+      data: {
+        country_sort: {
+          decrement: 1,
+        },
+      },
+    });
+  }
+
+  async changePopularCountry(
+    action: Action,
+    countryIds: number[],
+    email: string,
+  ) {
+    switch (action) {
+      case actionMap[Action.New]:
+        await this.addPopularCountry(countryIds, email);
+        break;
+      case actionMap[Action.SingleDelete]:
+        await this.deletePopularCountry(countryIds, email);
+        break;
+      case actionMap[Action.BatchDelete]:
+        await this.deletePopularCountry(countryIds, email);
+        break;
+      default:
+        throw new Error('Incorrect operation type');
+    }
+  }
+  /**
+   * @description 修改热门国家排序
+   * @memberof ProductService
+   */
+  async changeSort() {}
 }
